2d625fe73ba0c71614a7d674be8b04d48f04cdce
EllisG97 <ellisgraham97@hotmail.co.uk>
2022-07-01 13:38:45 +0100

    Uploaded Files

---
+++ compiler/ifelse.js
@@ -0,0 +1,11 @@
+// source file is ifelse.pdl
+var multiplyTwoNumbers = function multiplyTwoNumbers() {
+var option ;
+if( option == "P" ) {putstr("P") ;
+} else {if( option == "B" ) {putstr("B") ;
+} else {putstr("ERROR: Invalid Command!") ;
+ }
+ }
+}
+multiplyTwoNumbers();
+
---
+++ compiler/times.js
@@ -0,0 +1,9 @@
+// source file is times.pdl
+var multiplyTwoNumbers = function multiplyTwoNumbers() {
+var number1 ;
+putstr("Enter the first number: ") ;
+number1 = readline() ;
+putstr("result: "+number1+"\n") ;
+}
+multiplyTwoNumbers();
+
